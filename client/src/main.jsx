9b31c734e4c98fb1e961dc1e55a172b367afd91d
Mohit44357 <mohitk44357@gmail.com>
2024-04-03 10:08:44 +0530

    signin and signup toast added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { store } from './redux/store.js';
-import { provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <provider store={store}>
+  <React.StrictMode>
     <App />
-  </provider>,
+  </React.StrictMode>,
 )
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector} from 'react-redux';
-import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+
 
 export default function SignIn() {
 const [formData, setFormData ] = useState ({}); 
-const { loading, error } = useSelector((state) => state.user);
+const [error, setError] = useState(null);
+const [loading, setLoading] = useState(false);
 const navigate = useNavigate();
-const dispatch = useDispatch();
 const handleChange = (e) => {
   setFormData({
     ...formData,
@@ -17,7 +16,7 @@ const handleChange = (e) => {
 const handleSubmit = async (e) => {
      e.preventDefault();
      try {
-     dispatch(signInStart());
+     setLoading(true);
      const res = await fetch('/api/auth/signIn', {
       method: 'POST', 
         headers: {
@@ -28,13 +27,16 @@ const handleSubmit = async (e) => {
       const data = await res.json();
       console.log(data);
       if (data.success ===false) {
-        dispatch(signInFailure(data.message));
+        setLoading(false);
+        setError(data.message);
         return;
       } 
-      dispatch(signInSuccess(data));
+      setLoading(false);
+      setError(null);
       navigate('/');
     } catch (error) {
-      dispatch(signInFailure(error.message));
+      setLoading(false);
+      setError(error.message);
     }
       
 };
@@ -59,7 +61,7 @@ const handleSubmit = async (e) => {
         <span className='text-blue-700'>Sign up</span>
       </Link>
       </div>
-      {/* {error ? (
+      {error ? (
   error === "error from the function" ? (
     <p className='text-green-500 mt-5'>Already signed in</p>
   ) : (
@@ -68,6 +70,6 @@ const handleSubmit = async (e) => {
 ) : null}
 
 
-    </div> */}
+    </div>
   );
 }
